2b9c005f96a8d3d3c7304150bcf1999856950d30
Joshua Terrasas <joshua@terrasas.family>
2023-08-29 21:45:56 -0600

    Remove unused example code

--- src/main.ts
+++ src/main.ts
@@ -2,53 +2,28 @@ import {
   WorkspaceLeaf,
   ItemView,
   App,
-  Editor,
-  type MarkdownFileInfo,
-  MarkdownView,
-  Modal,
   Plugin,
-  PluginSettingTab,
-  Setting,
   type PluginManifest,
 } from "obsidian";
 import TaskListComponent from "./list.svelte";
 
-interface Settings {
-  setting: string;
-}
-
-const DEFAULT_SETTINGS: Settings = {
-  setting: "default",
-};
-
 export default class Arbiter extends Plugin {
-  settings: Settings;
   view: TaskListView | null;
 
   constructor(app: App, manifest: PluginManifest) {
     super(app, manifest);
-    this.settings = DEFAULT_SETTINGS;
     this.view = null;
   }
 
   override async onload() {
-    console.log("!!!! onload() !!!!");
-    await this.loadSettings();
-
     this.registerView(TASK_LIST_VIEW, (leaf: WorkspaceLeaf) => {
       return (this.view = new TaskListView(leaf));
     });
 
-    // Create icon in the left ribbon
     this.addRibbonIcon("list-checks", "Task List", (_evt: MouseEvent) => {
       this.activateView();
     });
 
-    // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-    const statusBarItemEl = this.addStatusBarItem();
-    statusBarItemEl.setText("Status Bar Text");
-
-    // This adds a simple command that can be triggered anywhere
     this.addCommand({
       id: "open_task_list",
       name: "Open task list",
@@ -56,128 +31,21 @@ export default class Arbiter extends Plugin {
         this.activateView();
       },
     });
-
-    // This adds an editor command that can perform some operation on the current editor instance
-    this.addCommand({
-      id: "sample-editor-command",
-      name: "Sample editor command",
-      editorCallback: (
-        editor: Editor,
-        _ctx: MarkdownView | MarkdownFileInfo,
-      ) => {
-        console.log(editor.getSelection());
-        editor.replaceSelection("Sample Editor Command");
-      },
-    });
-    // This adds a complex command that can check whether the current state of the app allows execution of the command
-    this.addCommand({
-      id: "open-sample-modal-complex",
-      name: "Open sample modal (complex)",
-      checkCallback: (checking: boolean) => {
-        // Conditions to check
-        const markdownView =
-          this.app.workspace.getActiveViewOfType(MarkdownView);
-        if (markdownView) {
-          // If checking is true, we're simply "checking" if the command can be run.
-          // If checking is false, then we want to actually perform the operation.
-          if (!checking) {
-            new SampleModal(this.app).open();
-          }
-
-          // This command will only show up in Command Palette when the check function returns true
-          return true;
-        }
-        return false;
-      },
-    });
-
-    // This adds a settings tab so the user can configure various aspects of the plugin
-    this.addSettingTab(new SampleSettingTab(this.app, this));
-
-    // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-    // Using this function will automatically remove the event listener when this plugin is disabled.
-    this.registerDomEvent(document, "click", (evt: MouseEvent) => {
-      console.log("click", evt);
-    });
-
-    // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-    this.registerInterval(
-      window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000),
-    );
   }
 
-  override onunload() {
-    console.log("!!!! onunload() !!!!");
-  }
-
-  async loadSettings() {
-    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-  }
-
-  async saveSettings() {
-    await this.saveData(this.settings);
-  }
+  override onunload() {}
 
   async activateView() {
-    console.log("!!!! activateView() !!!!");
     const views = this.app.workspace.getLeavesOfType(TASK_LIST_VIEW);
 
-    if(views.length) {
-      // this.app.workspace.revealLeaf(views[0]!);
-      console.log("!!!! views found !!!!");
+    if (views.length) {
       return;
     }
-
-    // this.app.workspace.detachLeavesOfType(TASK_LIST_VIEW);
 
     await this.app.workspace.getLeaf(true).setViewState({
       type: TASK_LIST_VIEW,
       active: true,
     });
-  }
-}
-
-class SampleModal extends Modal {
-  constructor(app: App) {
-    super(app);
-  }
-
-  override onOpen() {
-    const { contentEl } = this;
-    contentEl.setText("Woah!");
-  }
-
-  override onClose() {
-    const { contentEl } = this;
-    contentEl.empty();
-  }
-}
-
-class SampleSettingTab extends PluginSettingTab {
-  plugin: Arbiter;
-
-  constructor(app: App, plugin: Arbiter) {
-    super(app, plugin);
-    this.plugin = plugin;
-  }
-
-  display(): void {
-    const { containerEl } = this;
-
-    containerEl.empty();
-
-    new Setting(containerEl)
-      .setName("Setting #1")
-      .setDesc("It's a secret")
-      .addText((text) =>
-        text
-          .setPlaceholder("Enter your secret")
-          .setValue(this.plugin.settings.setting)
-          .onChange(async (value) => {
-            this.plugin.settings.setting = value;
-            await this.plugin.saveSettings();
-          }),
-      );
   }
 }
 
@@ -202,12 +70,9 @@ export class TaskListView extends ItemView {
     return "Task List";
   }
 
-  override async onOpen() {
-    console.log("!!!! onOpen() !!!!");
-  }
+  override async onOpen() {}
 
   override async onClose() {
-    console.log("!!!! onClose() !!!!");
     this.component.$destroy();
   }
 }
